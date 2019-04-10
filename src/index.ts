251c8530e598ea51c758de3ba0a132568b53f926
Ruben Medina <dev@rubenmedina.com>
2019-04-09 22:07:58 -0700

    Move OnStarAccessory back into index so Service and Characteristic are able to be used correctly

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,70 @@
-import OnStarAccessory from "./OnStarAccessory";
+import OnStar from "onstarjs";
+
+let Service: any, Characteristic: any;
 
 export default function(homebridge: any) {
-  homebridge.registerAccessory(
-    "homebridge-onstar",
-    "OnStar",
-    OnStarAccessory.bind(homebridge.hap.Service, homebridge.hap.Characteristic),
-  );
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-onstar", "OnStar", OnStarAccessory);
+}
+
+class OnStarAccessory {
+  private services: any[] = [];
+  private onStar: OnStar;
+
+  constructor(private log: Function, private config: any) {
+    this.services.push(this.createClimateService());
+
+    this.onStar = OnStar.create(config);
+  }
+
+  setOnStar(onStar: OnStar) {
+    this.onStar = onStar;
+  }
+
+  getServices() {
+    return this.services;
+  }
+
+  async getClimateOn(reply: Function) {
+    const climateState = false;
+
+    this.log("getClimateOn: Climate State: ", climateState ? "on" : "off");
+
+    reply(null, climateState);
+  }
+
+  async setClimateOn(on: boolean, reply: Function) {
+    this.log("setClimateOn: Turning on", on);
+
+    try {
+      this.log("setClimateOn: Requesting Remote Start");
+      const response = await this.onStar.remoteStart();
+      this.log("setClimateOn: Remote Start Finished", response.data);
+
+      reply();
+    } catch (e) {
+      if (e.response) {
+        this.log(`Error: ${e.response.status} - ${e.response.statusText}`);
+      } else if (e.request) {
+        this.log("Error: API returned no response");
+      } else {
+        this.log("Error:", e.message);
+      }
+
+      reply("Failed to start due to error");
+    }
+  }
+
+  private createClimateService() {
+    const climateService = new Service.Switch(this.config.name);
+
+    climateService
+      .getCharacteristic(Characteristic.On)
+      .on("get", this.getClimateOn.bind(this))
+      .on("set", this.setClimateOn.bind(this));
+
+    return climateService;
+  }
 }
