824e6d8ad45fa1ee4b4456b2770cb977780e08e8
reartvapps <ashtonalex5@icloud.com>
2024-11-23 12:11:27 -0500

    Create types.d.ts

---
+++ dist/types.d.ts
@@ -0,0 +1,27 @@
+export type OnStarJsMethod = "start" | "cancelStart" | "lockDoor" | "unlockDoor" | "alert" | "cancelAlert" | "chargeOverride";
+export declare enum OnStarAccessoryConfigKey {
+    DeviceId = "deviceId",
+    Vin = "vin",
+    Username = "username",
+    Password = "password",
+    OnStarPin = "onStarPin",
+    TOTPKEY = "TOTPKEY",
+    Name = "name",
+    EnableAlert = "enableAlert",
+    EnableCharger = "enableCharger",
+    EnableDoors = "enableDoors",
+    DoorsDefaultToUnlocked = "doorsDefaultToUnlocked"
+}
+export type OnStarAccessoryConfig = {
+    [OnStarAccessoryConfigKey.DeviceId]: string;
+    [OnStarAccessoryConfigKey.Vin]: string;
+    [OnStarAccessoryConfigKey.Username]: string;
+    [OnStarAccessoryConfigKey.Password]: string;
+    [OnStarAccessoryConfigKey.OnStarPin]: string;
+    [OnStarAccessoryConfigKey.TOTPKEY]: string;
+    [OnStarAccessoryConfigKey.Name]: string;
+    [OnStarAccessoryConfigKey.EnableAlert]?: boolean;
+    [OnStarAccessoryConfigKey.EnableCharger]?: boolean;
+    [OnStarAccessoryConfigKey.EnableDoors]?: boolean;
+    [OnStarAccessoryConfigKey.DoorsDefaultToUnlocked]?: boolean;
+};
