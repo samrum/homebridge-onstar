a21ff1a8d91bfd1ac34b2709eac4c00aa4381ef2
reartvapps <ashtonalex5@icloud.com>
2024-11-24 13:41:35 -0500

    Update index.mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -131,7 +131,7 @@ const REQUIRED_CONFIG_KEYS = [
     OnStarAccessoryConfigKey.Username,
     OnStarAccessoryConfigKey.Password,
     OnStarAccessoryConfigKey.OnStarPin,
-    OnStarAccessoryConfigKey.totpKey,
+    OnStarAccessoryConfigKey.onStarTOTP,
     OnStarAccessoryConfigKey.Name,
 ];
 function isValidConfig(config, log = () => { }) {
