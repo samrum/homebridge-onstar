8dae30adb1081ed33e861e80a9359d5b76dfad2a
reartvapps <ashtonalex5@icloud.com>
2024-11-26 17:25:31 -0500

    Update index.mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -166,7 +166,7 @@ class OnStarAccessory {
             username: this.config.username,
             password: this.config.password,
             onStarPin: this.config.onStarPin,
-            totpKey: this.config.Totpkey,
+            totpKey: this.config.totpKey,
             checkRequestStatus: false,
         }), this.log, this.hapCharacteristic, this.config.doorsDefaultToUnlocked || false);
         const name = this.config.name;
