a7c0f13f6eb9a5801d449aea37581693e598740b
reartvapps <ashtonalex5@icloud.com>
2024-11-26 19:34:30 -0500

    Update types.d.ts

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -5,7 +5,7 @@ export declare enum OnStarAccessoryConfigKey {
     Username = "username",
     Password = "password",
     OnStarPin = "onStarPin",
-    Totpkey = "totpKey",
+    onStarTOTP = "totpKey",
     Name = "name",
     EnableAlert = "enableAlert",
     EnableCharger = "enableCharger",
@@ -18,7 +18,7 @@ export type OnStarAccessoryConfig = {
     [OnStarAccessoryConfigKey.Username]: string;
     [OnStarAccessoryConfigKey.Password]: string;
     [OnStarAccessoryConfigKey.OnStarPin]: string;
-    [OnStarAccessoryConfigKey.Totpkey]: string;
+    [OnStarAccessoryConfigKey.onStarTOTP]: string;
     [OnStarAccessoryConfigKey.Name]: string;
     [OnStarAccessoryConfigKey.EnableAlert]?: boolean;
     [OnStarAccessoryConfigKey.EnableCharger]?: boolean;
