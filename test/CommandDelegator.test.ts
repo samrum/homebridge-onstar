a40e3fddb501d3969f6bde29689053cce20e73f6
reartvapps <ashtonalex5@icloud.com>
2024-12-01 19:44:16 -0500

    Update CommandDelegator.test.ts

--- test/CommandDelegator.test.ts
+++ test/CommandDelegator.test.ts
@@ -1,9 +1,9 @@
 import { mock, instance, when } from "ts-mockito";
 
-import OnStar from "onstarjs";
+import OnStar from "onstarjs2";
 import { HapCharacteristic, HapService } from "./hapMocks";
 import CommandDelegator from "../src/CommandDelegator";
-jest.mock("onstarjs");
+jest.mock("onstarjs2");
 jest.mock("../src/utils");
 
 let commandDelegator: CommandDelegator;
