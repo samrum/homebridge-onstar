b912c4098223e2fa5c552c93a83df300caad67f4
reartvapps <ashtonalex5@icloud.com>
2024-12-04 16:01:23 -0500

    Update ConfigValidation.ts

--- src/ConfigValidation.ts
+++ src/ConfigValidation.ts
@@ -6,7 +6,7 @@ const REQUIRED_CONFIG_KEYS = [
   OnStarAccessoryConfigKey.Username,
   OnStarAccessoryConfigKey.Password,
   OnStarAccessoryConfigKey.OnStarPin,
-  OnStarAccessoryConfigKey.onStarTOTP,
+  OnStarAccessoryConfigKey.OnStarTOTP,
   OnStarAccessoryConfigKey.Name,
 ];
 
